9588a2ae5df8df6a01671b2589457fcb73a00406
Zachary Kuchar <zacharyk323@live.com>
2023-08-09 14:17:48 -0400

    Applied clock logic

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+const secHand = document.querySelector(".sec-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
 function setDate() {
-  console.log("hi");
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const mins = now.getMinutes();
+  const hours = now.getHours();
+
+  const secondsDeg = (seconds / 60) * 360 + 90;
+  const minsDeg = (mins / 60) * 360 + 90;
+  const hoursDeg = (hours / 12) * 360 + 90;
+
+  secHand.style.transform = `rotate(${secondsDeg}deg)`;
+  minHand.style.transform = `rotate(${minsDeg}deg)`;
+  hourHand.style.transform = `rotate(${hoursDeg}deg)`;
+
+  console.log(seconds);
 }
 
 setInterval(setDate, 1000);
